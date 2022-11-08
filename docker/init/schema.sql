ac78d4be7185290756f5daf6a2383336b712a825
TRAN Juliette <tran.juliette.a@gmail.com>
2022-11-08 19:20:03 +0100

    Forgot some files

---
+++ docker/init/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS `fizzbuzz` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+USE `fizzbuzz`;
+
+CREATE TABLE IF NOT EXISTS `requests` (
+    `route` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+    `method` varchar(10) COLLATE utf8mb4_bin NOT NULL,
+    `queries` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+    `score` int NOT NULL DEFAULT '1',
+    PRIMARY KEY (`route`,`method`,`queries`) USING BTREE
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
